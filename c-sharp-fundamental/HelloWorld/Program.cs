da8be910b4767f008c975fd77229859453b7effc
S-O <s@example.com>
2023-10-03 11:10:16 +0530

    new project added for 3. primtive data types

--- c-sharp-fundamental/HelloWorld/Program.cs
+++ c-sharp-fundamental/HelloWorld/Program.cs
@@ -2,26 +2,3 @@
 
 // 2.Introduction to C# and .NET Framework
 Console.WriteLine("Hello, World!");
-
-// 3.Primitive Types and Expressions
-byte number = 2;
-var number1 = 3;
-int count = 10;
-float totalPrice = 20.95f;
-char character = 'A';
-string firstName = "Sanjay";
-bool isWorking = true;
-
-Console.WriteLine(number);
-Console.WriteLine(number1);
-Console.WriteLine(count);
-Console.WriteLine(totalPrice);
-Console.WriteLine(character);
-Console.WriteLine(firstName);
-Console.WriteLine(isWorking);
-
-Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
-Console.WriteLine("{0} {1}", float.MinValue, float.MaxValue);
-
-const float Pi = 3.14f;
-Console.WriteLine(Pi);
