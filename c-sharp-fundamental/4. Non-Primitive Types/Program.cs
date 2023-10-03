1a90651c8dba9919c88f9d686a77029cf323de3d
S-O <s@example.com>
2023-10-03 12:17:20 +0530

    Array 4. Non-Primirive Types

--- c-sharp-fundamental/4. Non-Primitive Types/Program.cs
+++ c-sharp-fundamental/4. Non-Primitive Types/Program.cs
@@ -7,6 +7,9 @@ internal class Program
 {
     private static void Main(string[] args)
     {
+        // Class
+        Console.WriteLine("Class");
+
         Person p1 = new Person();
         p1.FirstName = "Sanjay";
         p1.LastName = "Patel";
@@ -18,5 +21,29 @@ internal class Program
 
         var result1 = Calculator.AddStatic(2, 3);
         Console.WriteLine(result1);
+
+        // Array
+        Console.WriteLine("\nArray");
+
+        int[] numbers = new int[3];
+        numbers[0] = 1;
+
+        Console.WriteLine(numbers[0]);
+        Console.WriteLine(numbers[1]);
+        Console.WriteLine(numbers[2]);
+
+        //bool flags = new bool[3];
+        var flags = new bool[3];
+        flags[0] = true;
+
+        Console.WriteLine(flags[0]);
+        Console.WriteLine(flags[1]);
+        Console.WriteLine(flags[2]);
+
+        string[] names = new string[3] { "Sanjay", "Vivek", "Jaydeep" };
+
+        Console.WriteLine(names[0]);
+        Console.WriteLine(names[1]);
+        Console.WriteLine(names[2]);
     }
 }
